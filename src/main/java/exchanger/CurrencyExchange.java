b5406b0c932a22555fcf60b20179c266709d92d8
Karolis Šlipaitis <slipaitis.karolis@gmail.com>
2023-05-15 19:10:27 +0300

    move out currencies map printing of the reader

--- src/main/java/exchanger/CurrencyExchange.java
+++ src/main/java/exchanger/CurrencyExchange.java
@@ -3,6 +3,7 @@ package exchanger;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Map;
 
 public class CurrencyExchange {
 
@@ -22,9 +23,18 @@ public class CurrencyExchange {
         String currencyTo = args[3];
 
         HashMap<String, BigDecimal> currencyRates = new CurrencyRatesCsvReader(fileName).readCurrencyRates();
+        printCurrencies(currencyRates);
 
         BigDecimal exchangedAmount = new Exchanger(currencyRates).exchange(amount, currencyFrom, currencyTo);
         System.out.println("Result: " + exchangedAmount);
+    }
+
+    private static void printCurrencies(HashMap<String, BigDecimal> currencies) {
+        System.out.println();
+        for (Map.Entry<String, BigDecimal> entry : currencies.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        System.out.println();
     }
 
 }
