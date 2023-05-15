61ac080e3ed25b4af22ac5509e618a3eeb9fa44e
Karolis Šlipaitis <slipaitis.karolis@gmail.com>
2023-05-15 22:08:28 +0300

    add service layer with exception response wrapper

--- src/main/java/exchanger/controller/CurrencyExchangeController.java
+++ src/main/java/exchanger/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package exchanger.controller;
 
+import exchanger.service.CurrencyExchangeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class CurrencyExchangeController {
 
+    @Autowired
+    private CurrencyExchangeService currencyExchangeService;
+
     @PostMapping("/exchange")
     public ResponseEntity<String> exchange(@RequestBody String request) {
-        return new ResponseEntity<>("success", HttpStatus.OK);
+        try {
+            String response = currencyExchangeService.exchange(request);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/test/java/exchanger/controller/CurrencyExchangeControllerTest.java
+++ src/test/java/exchanger/controller/CurrencyExchangeControllerTest.java
@@ -1,11 +1,14 @@
 package exchanger.controller;
 
+import exchanger.service.CurrencyExchangeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,8 +19,14 @@ class CurrencyExchangeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private CurrencyExchangeService service;
+
     @Test
-    void exchange() throws Exception {
+    void exchange_success() throws Exception {
+        when(service.exchange("{}"))
+                .thenReturn("success");
+
         mockMvc.perform(
                         post("/api/exchange")
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -25,5 +34,19 @@ class CurrencyExchangeControllerTest {
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().string("success"));
+    }
+
+    @Test
+    void exchange_failure() throws Exception {
+        when(service.exchange("{}"))
+                .thenThrow(RuntimeException.class);
+
+        mockMvc.perform(
+                        post("/api/exchange")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{}")
+                )
+                .andExpect(status().is5xxServerError())
+                .andExpect(content().string(""));
     }
 }
