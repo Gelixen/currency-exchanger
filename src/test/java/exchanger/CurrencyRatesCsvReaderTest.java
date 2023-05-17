1d2a5247d862f486f9f8656aa848d4df25f420fc
Karolis Šlipaitis <slipaitis.karolis@gmail.com>
2023-05-17 19:27:11 +0300

    rename test class to a proper name

--- src/test/java/exchanger/CurrencyRatesCsvReaderTest.java
+++ src/test/java/exchanger/CurrencyRatesCsvReaderTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CurrencyRatesReaderTest {
+public class CurrencyRatesCsvReaderTest {
 
     private CurrencyRatesReader currencyRatesReader;
 
